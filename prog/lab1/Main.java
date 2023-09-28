2136bb5cef5ebe78df0e06ea7bc58a7d7a115134
godboydamn <duflyt@gmail.com>
2023-09-28 14:12:51 +0300

    edit code

--- prog/lab1/Main.java
+++ prog/lab1/Main.java
@@ -1,45 +1,60 @@
 import static java.lang.Math.*;
-import java.util.Random;
 
 public class Main {
-    public static void main(String[] args){
-        short[] c = new short[8];
-        float[] x = new float[17];
-        double[][] s = new double[8][17];
-        
-        Random random = new Random();
-        int currentNumber = 17;
-        
-        for (int i=0;i <c.length;i++){
-            c[i] = (short) currentNumber;
-            currentNumber -=2;
+    
+    private static final int C_SIZE = 8;
+    private static final int X_SIZE = 17;
+    
+    private static short[] createC() {
+    	short[] c = new short[C_SIZE];
+    	int currentNumber = 17;
+    
+    	for (int i = 0; i < c.length; i++){
+    		c[i] = (short) currentNumber;
+    		currentNumber -=2;
+    	}
+    
+    	return c;
+    
+    }
+    
+    private static float[] createX(){
+    	float[] x = new float[X_SIZE];
+    
+    	for (int i=0;i<x.length;i++){
+            x[i] = (float)(random() * 17 - 14);
         }
         
-        for (int i=0;i<x.length;i++){
-            x[i] = random.nextFloat() * 17 - 14;
-        }
+        return x;
         
-        
-        for (int i=0;i<7;i++){
-            for (int j=0;j<17;j++){
-                switch (c[i]){
+    }
+    
+    private static double calculateS(int ci, float xj){
+    	switch (ci){
                     case 3,5,7,17:
-                        s[i][j] = tan(atan(pow(((x[j]-5.5)/17), 2)));
-                        break;
+                        return tan(atan(pow(((xj-5.5)/17), 2)));
                     case 11:
-                        s[i][j] = pow((asin(1/exp(abs(x[j])))*(pow(atan((x[j]-5.5)/17),1/3 ))),pow(pow(x[j],x[j]/2),1/3));
-                    break;
+                    	return pow((asin(1/exp(abs(xj)))*(pow(atan((xj-5.5)/17),1/3 ))),pow(pow(xj,xj/2),1/3));
                     default:
-                        s[i][j] = (3/4)/(4+cos(pow(1/4+(x[j]-1)/x[j],2)));
+                        return (3/4)/(4+cos(pow(1/4+(xj-1)/xj,2)));
                         
-                }
-            }
+    	}
+    }
+    
+    public static void main(String[] args){
+        short[] c = createC();
+        float[] x = createX();
+        double[][] s = new double[C_SIZE][X_SIZE];
+        
+        for (int i = 0; i < C_SIZE; i++){
+        	for(int j = 0; j < X_SIZE; j++) {
+        	s[i][j] = calculateS(c[i], x[j]);
+        	}
         }
         
-        for (int i=0;i<7;i++){
-            for (int j=0;j<17;j++){
-                System.out.printf("%2.2f",s[i][j]);
-                System.out.print(" ");
+        for (int i = 0; i < C_SIZE; i++){
+            for (int j = 0; j < X_SIZE; j++){
+                System.out.printf("%2.2f ",s[i][j]);
             }
             System.out.println();
         }
